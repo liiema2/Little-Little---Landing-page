e132cd022acf992f79180275b337d2af8704a817
liiema2 <hieuliem72@gmail.com>
2023-04-10 08:43:03 +0700

    day1

---
+++ resources/views/navigation/navigation.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet/less" type="text/css" href="{{ ('assets/less/headding.less') }}" />
+<link rel="stylesheet" href="{{('assets/css/navigation.css')}}">
+{{-- <link rel="stylesheet" href="{{'assets/fonts/Montserrat/Montserrat.ttf'}}"> --}}
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <nav>
+        <div class="nav-logo-page"> <img src="{{url('assets/imgs/logo/logo.png')}}" alt=""> </div>
+        <div class="nav-information-page">
+            <div class="nav-hompage">
+                Trang chủ
+            </div>
+            <div class="nav-event">
+                Sự kiện
+            </div>
+            <div class="nav-contact">
+                Liên hệ
+            </div>
+
+        </div>
+        <div class="nav-number">
+            <img src="{{url('assets/imgs/icons/Frame 21.png')}}" alt="">
+            <p>28291393401</p>
+        </div>
+    </nav>
+</header>
+<div class="body-conter">
+    <img src="{{url('assets/imgs/background/bg.png')}}" alt="">
+
+
+    <div class="logo-damsen">
+        <img src="{{url('assets/imgs/logo/image 2.png')}}" alt="">
+    </div>
+    <div class="icons-left">
+        <img src="{{url('assets/imgs/icons/18451 [Converted]-06 1.png')}}" alt="">
+    </div>
+</div>
+<footer>
+
+</footer>
+</body>
+</html>
